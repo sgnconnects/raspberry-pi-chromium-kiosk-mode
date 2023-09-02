906221e2998e3fe4d3e2df87064a69583e35d6da
Zakhele Dlamini <97408905+sgnconnects@users.noreply.github.com>
2023-09-02 05:25:54 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
 
+mv  .config/lxsession/LXDE-virtnode/autostart
 sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt-get install matchbox x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3
